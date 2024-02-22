67454ddfc120bfb46153d68b15d907137b15a6b1
Filip Kotanski <filip.kotanski@gmail.com>
2024-02-22 15:20:17 +0000

    Modified base property in vite.config.js.

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Portfolio-In-React",
+  base: "/Portfolio-In-React/",
 })
